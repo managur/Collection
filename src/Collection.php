31258f0dff473e6bddfbaaede7e78463524cad81
Matt Brunt <mbrunt@helical-levity.com>
2019-06-24 17:33:48 +0100

    Add slice functionality into the collection

--- src/Collection.php
+++ src/Collection.php
@@ -200,6 +200,25 @@ class Collection extends ArrayObject implements JsonSerializable
     }
 
     /**
+     * Slice the sequence of elements from the array as per the `$offset` and `$length`
+     *
+     * @see https://www.php.net/manual/en/function.array-slice.php
+     *
+     * @param int $offset   If offset is non-negative, the sequence will start at that offset in the array.
+     *                      If offset is negative, the sequence will start that far from the end of the array.
+     *                      The offset parameter denotes the position in the array, not the key.
+     * @param int $length   If length is given and is positive, then the sequence will have up to that many elements in it.
+     *                      If the array is shorter than the length, then only the available array elements will be present.
+     *                      If length is given and is negative then the sequence will stop that many elements from the end of the array.
+     *                      If it is omitted, then the sequence will have everything from offset up until the end of the array.
+     * @return static New collection of the same type
+     */
+    public function slice(int $offset, int $length = null): self
+    {
+        return $this->getNewInstance(array_slice($this->getArrayCopy(), $offset, $length));
+    }
+
+    /**
      * Walk Over Collection Entities
      *
      * Does not return; use map() for that
