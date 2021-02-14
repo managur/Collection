b24e0d7954a2fe8c423488432fc5868d2aa058fe
Anthony Chambers <anthony@managur.io>
2021-02-14 23:53:35 +0000

    refactor: import collection interface for funcs

--- src/functions.php
+++ src/functions.php
@@ -1,16 +1,17 @@
 <?php
 
 use Managur\Collection\ArrayCollection;
+use Managur\Collection\Collection;
 
 if (!function_exists('collect')) {
-    function collect(mixed $items): \Managur\Collection\Collection
+    function collect(mixed $items): Collection
     {
         return new ArrayCollection($items);
     }
 }
 
 if (!function_exists('collectInto')) {
-    function collectInto(string $collectionType, mixed $items): \Managur\Collection\Collection
+    function collectInto(string $collectionType, mixed $items): Collection
     {
         return new $collectionType($items);
     }
